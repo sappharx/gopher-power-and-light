abe076ab5ce3edc3681b54dba290690514e3eb7f
sappharx <vinnysisk@gmail.com>
2015-07-27 15:33:42 -0600

    refactor to put functionality into separate functions

--- main.go
+++ main.go
@@ -12,29 +12,46 @@ func main() {
 	
 	gridLoad := 75.
 	
+	if option, err := requestOption(); err == nil {
+		switch option {
+		case "1":
+			generatePlantCapacityReport(plantCapacities...)
+		case "2":
+			generateGridLoadReport(activePlants, plantCapacities, gridLoad)
+		}
+	} else {
+		fmt.Println(err.Error())
+	}
+	
+}
+
+func requestOption() (option string, err error) {
 	fmt.Println("1) Generate Power Plant Report")
 	fmt.Println("2) Generate Power Grid Report")
 	fmt.Print("Please choose an option: ")
-	
-	var option string
-	
+		
 	fmt.Scanln(&option)
 	
-	switch option {
-		case "1":
-			for idx, cap := range plantCapacities {
-				fmt.Printf("Plant %d capacity: %.0f\n", idx, cap)
-			}
-		case "2":
-			capacity := 0.
-			for _, plantId := range activePlants {
-				capacity += plantCapacities[plantId]
-			}
-			
-			fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
-			fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
-			fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad / capacity * 100)
-		default:
-			fmt.Println("Unknown option, no action taken")
+	if option != "1" && option != "2" {
+		err = errors.New("Invalid option selected")
 	}
+	
+	return
+}
+
+func generatePlantCapacityReport(plantCapacities ...float64) {
+	for idx, cap := range plantCapacities {
+		fmt.Printf("Plant %d capacity: %.0f\n", idx, cap)
+	}
+}
+
+func generateGridLoadReport(activePlants []int, plantCapacities []float64, gridLoad float64) {
+	capacity := 0.
+	for _, plantId := range activePlants {
+		capacity += plantCapacities[plantId]
+	}
+	
+	fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
+	fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
+	fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad / capacity * 100)
 }
