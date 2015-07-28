823ca6bb870281c00de84112a57f7992181ec398
sappharx <vinnysisk@gmail.com>
2015-07-28 17:12:04 -0600

    add structs and methods for handling data (add OOP)

--- main.go
+++ main.go
@@ -1,23 +1,32 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 )
 
 func main() {
 	
-	plantCapacities := []float64{30, 30, 30, 60, 60, 100}
-	
+	plants := []PowerPlant{
+		PowerPlant{hydro, 300, active},
+		PowerPlant{wind, 30, active},
+		PowerPlant{wind, 25, inactive},
+		PowerPlant{wind, 35, active},
+		PowerPlant{solar, 45, unavailable},
+		PowerPlant{solar, 40, inactive},
+	}
+		
 	activePlants := []int{0, 1}
 	
-	gridLoad := 75.
+	grid := PowerGrid{300, plants}
 	
 	if option, err := requestOption(); err == nil {
 		switch option {
 		case "1":
-			generatePlantCapacityReport(plantCapacities...)
+			grid.generatePlantReport()
 		case "2":
-			generateGridLoadReport(activePlants, plantCapacities, gridLoad)
+			grid.generateGridReport()
 		}
 	} else {
 		fmt.Println(err.Error())
@@ -54,4 +63,60 @@ func generateGridLoadReport(activePlants []int, plantCapacities []float64, gridL
 	fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
 	fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
 	fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad / capacity * 100)
+}
+
+type PlantType string
+
+const (
+	hydro PlantType = "Hydro"
+	wind PlantType = "Wind"
+	solar PlantType = "Solar"
+)
+
+type PlantStatus string
+
+const (
+	active PlantStatus = "Active"
+	inactive PlantStatus = "Inactive"
+	unavailable PlantStatus = "Unavailable"
+)
+
+type PowerPlant struct {
+	plantType PlantType
+	capacity float64
+	status PlantStatus
+}
+
+type PowerGridt struct {
+	load float64
+	plants []PowerPlant
+}
+
+func (pg *PowerGrid) generatePlantReport() {
+	for idx, p := range pg.plants {
+		label := fmt.Sprintf("%s%d", "Plant #", idx)
+		fmt.Println(label)
+		fmt.Println(strings.Repeat("-", len(label)))
+		fmt.Printf("%-20s%s\n", "Type: ", p.PlantType)
+		fmt.Printf("%-20s%.0f\n", "Capacity: ", p.capacity)
+	 	fmt.Printf("%-20s%s\n", "Status: ", p.PlantStatus)
+		fmt.Println("")
+	}
+}
+
+func (pg *PowerGrid) generateGridReport() {
+	capacity := 0.
+	for _, p := range pg.plants {
+		if p.status == active {
+			capacity += p.capacity
+		}
+	}
+	
+	label := "Power Grid Report"
+	fmt.Println(label)
+	fmt.Println(strings.Repeat("-", len(label)))
+	fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
+	fmt.Printf("%-20s%.0f\n", "Load: ", pg.load)
+	fmt.Printf("%-20s%.1f%%\n", "Utilization: ", pg.load / capacity * 100)
+	
 }
